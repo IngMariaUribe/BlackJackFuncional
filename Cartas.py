5e70e8784efe705ba6ab8714686b94c20a1e0f8a
YeimerSN <52164044+YeimerSN@users.noreply.github.com>
2020-05-28 17:42:51 -0500

    Update Cartas.py

--- Cartas.py
+++ Cartas.py
@@ -42,10 +42,12 @@ def valor_mano_recargador(mano):
 def jugar(mazo, jugador, repartidor):
     print("Mano del jugador ", " "*5, jugador, " "*6,valor_mano_recargador(jugador))
     print("Mano del repartidor ", " "*2, repartidor, " "*6,valor_mano_recargador(repartidor))
-    
-    if len(mazo) > 2 and valor_mano_recargador(jugador) < 21 and valor_mano_recargador(repartidor) < 21:
-        return jugar(mazo[2:], jugador+[mazo[0]], repartidor+[mazo[1]])
-    ganador(jugador, repartidor)
+    if jugador == [] and repartidor == []:
+        jugar(mazo[4:], jugador+[mazo[0]] + [mazo[1]], repartidor+[mazo[2]] + [mazo[3]])
+    elif len(mazo) > 2 and valor_mano_recargador(jugador) < 21 and valor_mano_recargador(repartidor) < 21:
+        return jugar(mazo[4:], jugador+[mazo[0]], repartidor+[mazo[1]])
+    if valor_mano_recargador(jugador) > 21 or valor_mano_recargador(repartidor) > 21:
+        ganador(jugador, repartidor)
     
 def ganador(jugador, repartidor):
     if valor_mano_recargador(jugador) == 21 and valor_mano_recargador(repartidor) < 21:
@@ -61,7 +63,7 @@ def ganador(jugador, repartidor):
     elif 21 - valor_mano_recargador(repartidor) < 21 - valor_mano_recargador(jugador):
         print("Gana repartidor")
     elif valor_mano_recargador(jugador) == valor_mano_recargador(repartidor):
-        print("Empate")
+        print("Gana repartidor")
 
 
 jugar(mezclar(baraja()), [], [])
